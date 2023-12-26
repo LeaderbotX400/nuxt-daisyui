31e2729c47be082a78031546e040ade012de1b07
LeaderbotX400 <eric@singerfamily.ca>
2023-12-26 06:42:58 -0700

    Add VS Code configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
   devtools: {
     enabled: true,
+
     timeline: {
       enabled: true,
     },
+
+    sourceMapsEnabled: true,
   },
 
   experimental: {
